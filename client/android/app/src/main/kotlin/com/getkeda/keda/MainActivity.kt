de187ae776c984afb84bff878a2fed1a84189708
Sebastian Choren <schoren@users.noreply.github.com>
2026-01-21 16:42:29 -0300

    fix security warnings (#30)

--- client/android/app/src/main/kotlin/com/getkeda/keda/MainActivity.kt
+++ client/android/app/src/main/kotlin/com/getkeda/keda/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.getkeda.keda
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Prevent screenshots and screen recording
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+        // Protect against tapjacking (overlays)
+        window.decorView.filterTouchesWhenObscured = true
+    }
+}
