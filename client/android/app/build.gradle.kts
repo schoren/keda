a398bb27d0998a8a4f4908d7437cece160f70db5
Sebastian Choren <sebastian.choren@gmail.com>
2026-02-05 15:53:20 -0300

    fix security

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.getkeda.keda"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 29
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
